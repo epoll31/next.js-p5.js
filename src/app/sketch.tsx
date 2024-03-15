c7dbfc499776e1a70f95d2407fe1af6323f2485c
ethan <epollack31@gmail.com>
2024-03-15 12:28:57 -0700

    basic flocking working

--- src/app/sketch.tsx
+++ src/app/sketch.tsx
@@ -13,31 +13,199 @@ interface ComponentProps {
     // Your component props
 }
 
-let x = 50;
-let y = 50;
-let radius = 50;
-let angle = 30;
-let speed = 1;
+let flock: Flock;
+
+class Flock {
+    boids: Boid[];
+
+    constructor() {
+        this.boids = [];
+    }
+
+    run(p5: p5Types) {
+        for (let boid of this.boids) {
+            boid.run(p5, this.boids);
+        }
+    }
+
+    addBoid(b: Boid) {
+        this.boids.push(b);
+    }
+}
+
+class Boid {
+    acceleration: p5Types.Vector;
+    velocity: p5Types.Vector;
+    position: p5Types.Vector;
+    r: number;
+    maxspeed: number;
+    maxforce: number;
+
+    constructor(x: number, y: number) {
+        this.acceleration = new p5Types.Vector(0, 0);
+        this.velocity = p5Types.Vector.random2D();
+        this.position = new p5Types.Vector(x, y);
+        this.r = 3.0;
+        this.maxspeed = 3;
+        this.maxforce = 0.05;
+    }
+
+    run(p5: p5Types, boids: Boid[]) {
+        this.flock(p5, boids);
+        this.update(p5);
+        this.borders(p5);
+        this.render(p5);
+    }
+
+    applyForce(force: p5Types.Vector) {
+        this.acceleration.add(force);
+    }
+
+    flock(p5: p5Types, boids: Boid[]) {
+        let sep = this.separate(p5, boids);
+        let ali = this.align(p5, boids);
+        let coh = this.cohesion(p5, boids);
+
+        sep.mult(1.5);
+        ali.mult(1.0);
+        coh.mult(1.0);
+
+        this.applyForce(sep);
+        this.applyForce(ali);
+        this.applyForce(coh);
+    }
+
+    update(p5: p5Types) {
+        this.velocity.add(this.acceleration);
+        this.velocity.limit(this.maxspeed);
+        this.position.add(this.velocity);
+        this.acceleration.mult(0);
+    }
+
+    seek(p5: p5Types, target: p5Types.Vector) {
+        let desired = p5Types.Vector.sub(target, this.position);
+        desired.setMag(this.maxspeed);
+
+        let steer = p5Types.Vector.sub(desired, this.velocity);
+        steer.limit(this.maxforce);
+
+        return steer;
+    }
+
+    render(p5: p5Types) {
+        let theta = this.velocity.heading() + p5.PI / 2;
+        p5.fill(127);
+        p5.stroke(200);
+        p5.push();
+        p5.translate(this.position.x, this.position.y);
+        p5.rotate(theta);
+        p5.beginShape();
+        p5.vertex(0, -this.r * 2);
+        p5.vertex(-this.r, this.r * 2);
+        p5.vertex(this.r, this.r * 2);
+        p5.endShape(p5.CLOSE);
+        p5.pop();
+    }
+
+    borders(p5: p5Types) {
+        if (this.position.x < -this.r) this.position.x = p5.width + this.r;
+        if (this.position.y < -this.r) this.position.y = p5.height + this.r;
+        if (this.position.x > p5.width + this.r) this.position.x = -this.r;
+        if (this.position.y > p5.height + this.r) this.position.y = -this.r;
+    }
+
+    separate(p5: p5Types, boids: Boid[]) {
+        let desiredseparation = 25.0;
+        let steer = new p5Types.Vector(0, 0);
+        let count = 0;
+
+        for (let other of boids) {
+            let d = p5Types.Vector.dist(this.position, other.position);
+
+            if (d > 0 && d < desiredseparation) {
+                let diff = p5Types.Vector.sub(this.position, other.position);
+                diff.normalize();
+                diff.div(d);
+                steer.add(diff);
+                count++;
+            }
+        }
+
+        if (count > 0) {
+            steer.div(count);
+        }
+
+        if (steer.mag() > 0) {
+            steer.setMag(this.maxspeed);
+            steer.sub(this.velocity);
+            steer.limit(this.maxforce);
+        }
+
+        return steer;
+    }
+    align(p5: p5Types, boids: Boid[]) {
+        let neighbordist = 50;
+        let sum = new p5Types.Vector(0, 0);
+        let count = 0;
+
+        for (let other of boids) {
+            let d = p5Types.Vector.dist(this.position, other.position);
+
+            if (d > 0 && d < neighbordist) {
+                sum.add(other.velocity);
+                count++;
+            }
+        }
+
+        if (count > 0) {
+            sum.div(count);
+            sum.setMag(this.maxspeed);
+            let steer = p5Types.Vector.sub(sum, this.velocity);
+            steer.limit(this.maxforce);
+            return steer;
+        } else {
+            return new p5Types.Vector(0, 0);
+        }
+    }
+    cohesion(p5: p5Types, boids: Boid[]) {
+        let neighbordist = 50;
+        let sum = new p5Types.Vector(0, 0);
+        let count = 0;
+
+        for (let other of boids) {
+            let d = p5Types.Vector.dist(this.position, other.position);
+
+            if (d > 0 && d < neighbordist) {
+                sum.add(other.position);
+                count++;
+            }
+        }
+
+        if (count > 0) {
+            sum.div(count);
+            return this.seek(p5, sum);
+        } else {
+            return new p5Types.Vector(0, 0);
+        }
+    }
+}
 
 const SketchComponent: React.FC<ComponentProps> = (props: ComponentProps) => {
 
     // See annotations in JS for more information
     const setup = (p5: p5Types, canvasParentRef: Element) => {
-        p5.createCanvas(500, 500).parent(canvasParentRef);
+        p5.createCanvas(700, 300).parent(canvasParentRef);
+
+        flock = new Flock();
+        for (let i = 0; i < 100; i++) {
+            let b = new Boid(p5.width / 2, p5.height / 2);
+            flock.addBoid(b);
+        }
     };
 
     const draw = (p5: p5Types) => {
-        x += speed * p5.cos(p5.radians(angle));
-        y += speed * p5.sin(p5.radians(angle));
-        if (x > p5.width - radius || x < radius) {
-            angle = 180 - angle;
-        }
-        if (y > p5.height - radius || y < radius) {
-            angle = 360 - angle;
-        }
-
-        p5.background(255);
-        p5.ellipse(x, y, radius * 2, radius * 2);
+        p5.background(51);
+        flock.run(p5);
     };
 
     return <Sketch setup={setup} draw={draw} />;
